bbadb0ee68d5e7728b88e9cbe1135e9fd8dcd52c
Rebecca Meredith <rebecca@that-redhead.com>
2013-05-20 18:39:39 -0700

    work work work

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var callback = function (playerShots) {
+    var html = '';
+
+    $('h2 b').text(playerShots.shots[0].player.name);
+
+    $.each(playerShots.shots, function (i, shot) {
+        html += '<li class="three columns">';
+        html += '<a href="' + shot.url + '" target="_blank">';
+        html += '<img src="' + shot.image_url + '" ';
+        html += 'alt="' + shot.title + '"></a></li>';
+    });
+
+    $('#shotsByPlayerId').html(html);
+};
+
+$.jribbble.getShotsByPlayerId('That-RedHead', callback, {page: 1, per_page: 12});
